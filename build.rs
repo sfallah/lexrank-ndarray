bbe7abd8ef493a77f4f4db377b0383a7667e977a
sfallah <s.fallah@qimia.de>
2024-09-13 15:50:03 +0200

    added blis deps (works on amd arch)

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() {
     #[cfg(all(target_os = "macos", feature = "accelerate"))]
     println!("cargo:rustc-link-lib=framework=Accelerate");
+    #[cfg(all(target_os = "linux", feature = "blas"))]
+    println!("cargo:rustc-link-lib=blas");
 }
