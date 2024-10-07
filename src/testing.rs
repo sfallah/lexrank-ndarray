77e07726ea56d2becfe21103308025f4301a96ea
Ralf Christian Kinkel <r.kinkel@qimia.de>
2024-10-07 13:37:58 +0200

    added size_of lib to testing.rs

--- src/testing.rs
+++ src/testing.rs
@@ -1,11 +1,12 @@
 use memmap2::MmapOptions;
-use ndarray::{Array, Array1, Array2};
+use ndarray::{Array, Array2};
 use ndarray_rand::rand_distr::Normal;
 use ndarray_rand::RandomExt;
 use safetensors::SafeTensors;
 use serde::{Deserialize, Serialize};
 use std::fs;
 use std::fs::File;
+use std::mem::size_of;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct SplitData {
