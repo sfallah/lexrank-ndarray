4fbb94d78c4833aa08898da4e6a1999858edb6e5
Saba Fallah <s.fallah@qimia.de>
2025-08-06 12:21:44 +0200

    benchmark with correct dataset

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -102,12 +102,15 @@ pub fn benches() {
         .configure_from_args();
 
     let data_set_map = [
+        //(
+        //    "MiniLM-L6-v2",
+        //    "tests/test_data/superlinear_embeddings/all-MiniLM-L6-v2/",
+        //),
         (
-            "MiniLM-L6-v2",
-            "tests/test_data/superlinear_embeddings/all-MiniLM-L6-v2/",
-        ),
-        //("bge-m3", "tests/test_data/superlinear_embeddings/bge-m3/"),
-        //("multilingual-e5-large-instruct", "tests/test_data/superlinear_embeddings/multilingual-e5-large-instruct/"),
+            "gte-Qwen2-1.5B-instruct",
+            "tests/test_data/superlinear_embeddings/gte-Qwen2-1.5B-instruct/",
+        ), //("bge-m3", "tests/test_data/superlinear_embeddings/bge-m3/"),
+           //("multilingual-e5-large-instruct", "tests/test_data/superlinear_embeddings/multilingual-e5-large-instruct/"),
     ];
     for (dataset, tensor_file) in data_set_map.iter() {
         //ndarray_normalize_l2(&mut criterion, dataset, tensor_file);
